8399d323f9d1bb4d8171dbf7f4947bd85e11fa96
dsa069 <dsa069@inlumine.ual.es>
2026-04-06 19:44:49 +0200

    Revert "Refactor greeting methods: enhance name formatting and capitalization in greet and greetLoud functions"
    This reverts commit 5e19219b6224c74d3a5daa74e694b79c7bf278a1.

--- src/services/hello-world.js
+++ src/services/hello-world.js
@@ -78,41 +78,6 @@ class HelloWordService {
                 return "Hello " + cleanName + "!";
       
     }
-
-        /**
-         * @description Says HELLO to a given name
-         * @param nameToHello {string} Name to greet
-         * @returns an uppercase greeting string
-         */
-        greetLoud ( nameToHello ) {
-                let cleanName = "";
-
-                if (typeof nameToHello === "string") {
-                    cleanName = nameToHello.trim();
-                    cleanName = cleanName.replace(/\s+/g, " ");
-
-                    if (cleanName.includes("_")) {
-                        cleanName = cleanName.split("_").join(" ");
-                    }
-
-                    cleanName = cleanName
-                        .split(" ")
-                        .map((part) => {
-                            if (!part) {
-                                return "";
-                            }
-
-                            return part.charAt(0).toUpperCase() + part.slice(1).toLowerCase();
-                        })
-                        .join(" ");
-                }
-
-                if (!cleanName) {
-                    cleanName = "World";
-                }
-
-                return ("Hello " + cleanName + "!").toUpperCase();
-        }
   }
   
   module.exports = HelloWordService;
